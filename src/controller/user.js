f7da661383286b530d310f8249487426e34c68fa
aha <223311795@qq.com>
2020-03-25 17:38:30 +0800

    退出登录 api

--- src/controller/user.js
+++ src/controller/user.js
@@ -17,7 +17,8 @@ const {
   registerFailInfo,
   loginFailInfo,
   deleteUserFailInfo,
-  changeInfoFailInfo
+  changeInfoFailInfo,
+  changePasswordFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 
@@ -144,10 +145,43 @@ async function changeUserInfo(ctx, {
   return new ErrorModel(changeInfoFailInfo)
 }
 
+/**
+ * 修改用户密码
+ * @param {string} userName 用户名
+ * @param {string} password 旧密码
+ * @param {string} newPassword 新密码
+ */
+async function changeUserPassword({ userName, password, newPassword }) {
+  const result = await updateUser(
+    { newPassword: doCrypto(newPassword) },
+    { userName, 
+      password: doCrypto(password)
+    }
+  )
+
+  if(result) {
+    return new SuccessModel()
+  }
+
+  return new ErrorModel(changePasswordFailInfo)
+
+}
+
+/**
+ * 退出登录
+ * @param {object} ctx 执行上下文
+ */
+async function logout(ctx) {
+  delete ctx.session.userInfo
+  return new SuccessModel()
+}
+
 module.exports = {
   isExist,
   regiseter,
   login,
   deleteCurUser,
-  changeUserInfo
+  changeUserInfo,
+  changeUserPassword,
+  logout
 }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,11 +3,25 @@
  */
 
 const router = require('koa-router')()
-const { isExist, regiseter, login, deleteCurUser, changeUserInfo } = require('../../controller/user')
+const {
+  isExist,
+  regiseter,
+  login,
+  deleteCurUser,
+  changeUserInfo,
+  changeUserPassword,
+  logout
+} = require('../../controller/user')
 const userValidate = require('../../validator/user')
-const { genValidator } = require('../../middlewares/validator')
-const { isTest } = require('../../utils/env')
-const { loginCheck } = require('../../middlewares/loginChecks')
+const {
+  genValidator
+} = require('../../middlewares/validator')
+const {
+  isTest
+} = require('../../utils/env')
+const {
+  loginCheck
+} = require('../../middlewares/loginChecks')
 
 router.prefix('/api/user')
 
@@ -16,37 +30,85 @@ router.prefix('/api/user')
 router.post('/register', genValidator(userValidate), async (ctx, next) => {
   //genValidator(userValidate) 校验数据
   //获取数据
-  const { userName, password, gender } = ctx.request.body 
+  const {
+    userName,
+    password,
+    gender
+  } = ctx.request.body
   //返回数据
-  ctx.body = await regiseter({ userName, password, gender })
+  ctx.body = await regiseter({
+    userName,
+    password,
+    gender
+  })
   //ctx.render() //view
 })
 
 //用户名是否存在
 router.post('/isExist', async (ctx, next) => {
-  const { userName } = ctx.request.body
+  const {
+    userName
+  } = ctx.request.body
   ctx.body = await isExist(userName)
 })
 
 //用户登陆
 router.post('/login', async (ctx, next) => {
-  const { userName, password } = ctx.request.body
-  ctx.body = await login({ctx, userName, password})
+  const {
+    userName,
+    password
+  } = ctx.request.body
+  ctx.body = await login({
+    ctx,
+    userName,
+    password
+  })
 })
 
 //用户删除
 router.post('/delete', loginCheck, async (ctx, next) => {
-  if(isTest) {
+  if (isTest) {
     //测试环境才提供删除用户操作，测试账号登录之后，删除自己
-    const { userName } = ctx.session.userInfo
+    const {
+      userName
+    } = ctx.session.userInfo
     ctx.body = await deleteCurUser(userName)
   }
 })
 
 //修改用户信息
 router.patch('/changeInfo', loginCheck, genValidator(userValidate), async (ctx, next) => {
-  const { nickName, picture, city } = ctx.request.body
-  ctx.body = await changeUserInfo(ctx, { nickName, picture, city })
+  const {
+    nickName,
+    picture,
+    city
+  } = ctx.request.body
+  ctx.body = await changeUserInfo(ctx, {
+    nickName,
+    picture,
+    city
+  })
+})
+
+//修改密码
+router.patch('/changePassword', loginCheck, genValidator(userValidate), async (ctx, next) => {
+  const {
+    password,
+    newPassword
+  } = ctx.request.body
+  const {
+    userName
+  } = ctx.session.userInfo
+  ctx.body = await changeUserPassword({
+    userName,
+    password,
+    newPassword
+  })
+})
+
+//退出登陆
+router.post('/logout', loginCheck, async (ctx, next) => {
+  ctx.body = await logout(ctx)
 })
 
 
