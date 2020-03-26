28c6ab360db7e643e86d8a3bd6d437ba591f655e
aha <223311795@qq.com>
2020-03-26 10:27:59 +0800

    微博数据模型

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -4,5 +4,5 @@
 
 
 module.exports = {
-  DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs'
+  DEFAULT_PICTURE: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1654409480,2232044795&fm=26&gp=0.jpg'
 }
